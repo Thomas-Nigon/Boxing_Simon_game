1553d46ec39e61e2d6c8c1412e0ddbf4514166b3
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2023-09-17 14:24:18 +0200

    before forking main to dev branch

--- scripts.js
+++ scripts.js
@@ -21,52 +21,66 @@ const closeModalG = document.querySelector('.good');
 const closeModalW = document.querySelector('.wrong');
 const closeModalAbout = document.querySelector('.btnAbout')
 
-closeModalG.addEventListener('click', ()=>{
+closeModalG.addEventListener('click', () => {
   modalGood.close()
   console.log("close modal good")
 })
-closeModalW.addEventListener('click', ()=>{
+closeModalW.addEventListener('click', () => {
   modalWrong.close()
   console.log("close modal wrong")
 })
-closeModalAbout.addEventListener('click', ()=>{
+closeModalAbout.addEventListener('click', () => {
   modalAbout.close()
   console.log("close modal about")
 })
-  
 
+function createItem() {
+  for (i = 0; i < punchSequence.length; i++) {
+    // create a new Item to our list
+    const newItem = document.createElement("li")
+    /*   newItem.classList.add('item') */
+
+    // add a name to that item:
+    const newName = document.createTextNode(punchSequence[i])
+    // add the text to the li element:
+    newItem.appendChild(newName)
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.querySelector("#prompt");
+    currentDiv.appendChild(newItem)
+  }
+}
 
 
 //FUNCTION START GAME
 const start = document.querySelector('[name="startButton"]')
-const list = document.querySelector('#prompt')
+const list = document.querySelector('.item')
 
-function startGame(){
- punchSequence = []
- console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
- for (let i = 0; i < player.level; i++){
- randomPunches = punchList[Math.floor(Math.random() * 4)]
- punchSequence.push(randomPunches)
- console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
- list.textContent = punchSequence
- }
+function startGame() {
+  punchSequence = []
+  console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
+  for (let i = 0; i < player.level; i++) {
+    randomPunches = punchList[Math.floor(Math.random() * 4)]
+    punchSequence.push(randomPunches)
+    console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
+    createItem()
+  }
 }
 
 
 // PAGE SLIDING FUNCTION
 const pages = document.querySelectorAll(".page");
-const translateAmount = 100; 
-let currentPage =0;
+const translateAmount = 100;
+let currentPage = 0;
 let translate = 0;
-function slide(direction){
+function slide(direction) {
   console.log("inside sliding function")
-  direction === "next" ? (translate -= translateAmount, currentPage --) : (translate += translateAmount, currentPage ++);
-  
+  direction === "next" ? (translate -= translateAmount, currentPage--) : (translate += translateAmount, currentPage++);
+
   pages.forEach(
     pages => (pages.style.transform = `translateX(${translate}%)`)
   );
   console.log(currentPage)
-  if (currentPage === -2){
+  if (currentPage === -2) {
     console.log('P2 start game please')
     startGame()
   }
@@ -77,20 +91,20 @@ const menu = document.querySelector('.menuIcon')
 const menuBar = document.querySelector('.menuBar')
 let activeMenu = 0
 function menuSlide() {
-  if(activeMenu) {
+  if (activeMenu) {
     menuBar.classList.remove('active')
     activeMenu = 0
   } else {
     menuBar.classList.add('active')
-    activeMenu = 1  
+    activeMenu = 1
   }
-  }
-  menu.addEventListener('click', menuSlide )
+}
+menu.addEventListener('click', menuSlide)
 
 // FILL NAME FUNCTION
 const nameForm = document.querySelector('input')
 
-function fillName(e){
+function fillName(e) {
   console.log(e)
   nameForm.value = ''
   nameForm.classList.add('active')
@@ -104,7 +118,7 @@ nameForm.addEventListener('click', fillName)
 const next = document.querySelector('input[type="submit"]')
 const playerName = document.querySelector('span')
 
-function toTuto(){
+function toTuto() {
   console.log(nameForm.value)
   player.name = nameForm.value
   playerName.textContent = player.name
@@ -114,46 +128,53 @@ next.addEventListener('click', toTuto)
 
 
 // PUNCH SEQUENCE GENERATOR
-function randomizePunches(){
+function randomizePunches() {
   punchSequence = []
   console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
-  for (let i = 0; i < player.level; i++){
-  randomPunches = punchList[Math.floor(Math.random() * 4)]
-  punchSequence.push(randomPunches)
-  console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
-  }  
-  list.textContent = punchSequence
+  for (let i = 0; i < player.level; i++) {
+    randomPunches = punchList[Math.floor(Math.random() * 4)]
+    punchSequence.push(randomPunches)
+    console.log(`function randomizePunches: punchSequence: ${punchSequence}`)
+  }
+  createItem()
 }
 
 //LVLUP AND RESET TO NEXT SEQUENCE FUNCTION
 const level = document.querySelector('[name="level"]')
 const winStreak = document.querySelector('[name="winStreak"]')
 const combo = document.querySelector('[name="combo"]')
-function lvlUpReset(){
+function lvlUpReset() {
   modalGood.showModal()
   console.log("entering level function")
-  seqCntr=0
+  seqCntr = 0
   punchSequence = []
   playerSequence = []
+
+  //select the list parent: the UL
+  const currentDiv = document.querySelector("#prompt");
+  //select all list items: LIs
+  const listItems = document.querySelector("li");
+  currentDiv.removeChild(listItems)
+  
   randomPunches = []
-  player.level+=1
+  player.level += 1
   level.textContent = player.level
-  if (player.level>= player.bestWinStreak){
+  if (player.level >= player.bestWinStreak) {
     player.bestWinStreak = player.level
     winStreak.textContent = player.bestWinStreak
   }
-  
-  player.comboDone+=1
+
+  player.comboDone += 1
   combo.textContent = " "
   combo.textContent = player.comboDone
   randomizePunches()
 }
 
 // RESET AFTER A GAMEOVER
-function resetGameOver(){
+function resetGameOver() {
   modalWrong.showModal()
   seqCntr = 0
-  player.level = 1 
+  player.level = 1
   punchSequence = []
   playerSequence = []
   randomPunches = []
@@ -163,40 +184,38 @@ function resetGameOver(){
 // MATCHING PLAYER INPUT AND RANDOM SEQ
 let misses = 0
 const missed = document.querySelector('[name="missed"]')
-function matchingCheck(){
+function matchingCheck() {
   console.log("entering matching check function")
-  console.log(`Sequence: ${seqCntr}/${player.level-1}, I clicked: ${playerSequence[seqCntr]}, expected: ${punchSequence[seqCntr]}`)
-  if (playerSequence[seqCntr] !== punchSequence[seqCntr]){
-  console.log("gameOver")
-  misses +=1
-  missed.textContent = ""
-  missed.textContent = misses 
-  resetGameOver()
-  
-  } else if (seqCntr === (punchSequence.length-1) ){
+  console.log(`Sequence: ${seqCntr}/${player.level - 1}, I clicked: ${playerSequence[seqCntr]}, expected: ${punchSequence[seqCntr]}`)
+  if (playerSequence[seqCntr] !== punchSequence[seqCntr]) {
+    console.log("gameOver")
+    misses += 1
+    missed.textContent = ""
+    missed.textContent = misses
+    resetGameOver()
+
+  } else if (seqCntr === (punchSequence.length - 1)) {
     console.log("goodJob combo done")
     lvlUpReset()
 
-    } else {
-      console.log("owiii")
-      seqCntr++
-    } 
+  } else {
+    console.log("owiii")
+    seqCntr++
+  }
 }
 
 //LISTENING TO PLAYER SEQUENCE
 const playerInput = document.querySelectorAll('.punches')
 playerInput.forEach(button => button.addEventListener('click', playerInputSequence))
-function playerInputSequence(e){
+function playerInputSequence(e) {
   console.log("entering player inputSequence")
   console.log("clearing punch display")
   console.log(e)
   console.log(`${e.srcElement.name}`)
-  list.textContent= ""
   playerSequence.push(e.srcElement.name)
   console.log(`I clicked ${playerSequence} button`)
-  list.textContent= playerSequence
   matchingCheck()
-} 
+}
 
 // RESET FUNCTION
 const reset = document.querySelector('.reset')
@@ -207,6 +226,6 @@ reset.addEventListener('click', () => {
 
 //ABOUT FUNCTION
 btnAbout.addEventListener('click', () => {
-modalAbout.showModal()
-console.log('show about')
+  modalAbout.showModal()
+  console.log('show about')
 })
